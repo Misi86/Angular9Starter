cb548f48956814b2042a24253c91d1d3f92370ce
Marco Isidori <marco.isidori@gmail.com>
2020-09-12 13:28:33 +0200

    update structure

---
+++ src/app/modules/home/indicators/indicators.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'dashboard-component',
+    templateUrl: './indicators.component.html',
+    styleUrls: ['./dashboard.component.scss'],
+})
+export class IndicatorsComponent{
+
+    constructor() {}
+}
+
---
+++ src/app/modules/home/strategies/strategies.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'dashboard-component',
+    templateUrl: './strategies.component.html',
+    styleUrls: ['./dashboard.component.scss'],
+})
+export class StrategiesComponent{
+
+    constructor() {}
+}
+
