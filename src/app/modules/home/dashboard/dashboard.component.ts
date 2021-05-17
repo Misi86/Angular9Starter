dc53c161b29db01285a2fe6e6e642f68d0b17869
Marco Isidori <marco.isidori@gmail.com>
2021-05-17 21:47:48 +0200

    update dash stuff

--- src/app/modules/home/dashboard/dashboard.component.ts
+++ src/app/modules/home/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {ModalComponent} from '../../../shared/modal/modal.component';
 
 @Component({
     selector: 'dashboard-component',
@@ -6,37 +7,60 @@ import {Component} from '@angular/core';
     styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent{
+  @ViewChild('confirmModal') confirmModal: ModalComponent;
     private activeStrategies: any;
     constructor() {
 
       this.activeStrategies = [
         {
           name: 'Fake  Strategies 1',
-          coin_pair: 'ETH/BTC',
+          coin_pair: 'BNB/BTC',
           indicator_id: 'FAKE_INDICATOR',
           timeframe: '1d',
-          budget: '25%',
+          initial_budget: '0.00000010',
+          current_budget: '0.00000016',
           created_at: '2020/09/01',
+          buy_price: 0.00005,
+          sell_price: 0.000051,
           is_active: 1,
         }, {
           name: 'Fake  Strategies 2',
           coin_pair: 'BNB/BTC',
           indicator_id: 'FAKE_INDICATOR',
           timeframe: '1d',
-          budget: '25%',
+          initial_budget: '0.00000011',
+          current_budget: '0.00000014',
           created_at: '2020/09/02',
+          buy_price: 0.000049,
+          sell_price: 0.000050,
           is_active: 1,
         }, {
           name: 'Fake  Strategies 3',
-          coin_pair: 'XLM/BTC',
+          coin_pair: 'BNB/BTC',
           indicator_id: 'FAKE_INDICATOR',
           timeframe: '1d',
-          budget: '25%',
+          initial_budget: '0.00000012',
+          current_budget: '0.00000015',
+          buy_price: 0.000052,
+          sell_price: 0.000053,
           created_at: '2020/09/03',
-          is_active: 0
+          is_active: 1
         }
       ];
 
     }
+
+  checkStrategy() {
+    this.openConfirmModal();
+
+  }
+
+  close() {
+    this.confirmModal.dismiss();
+  }
+
+  openConfirmModal() {
+    this.confirmModal.show('modal-lg');
+  }
 }
 
